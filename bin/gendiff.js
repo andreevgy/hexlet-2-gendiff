5ccd2bed39058aa92026f09ccd5ae4b2e80f4939
Gleb Andreev <gleb.andreev2000@gmail.com>
2021-11-01 20:49:10 +0300

    Added arguments and format type

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -3,10 +3,12 @@ import { program } from "commander";
 
 program
   .name('gendiff')
-  .usage('[options]')
+  .usage('[options] <filepath1> <filepath2>')
+  .arguments('<filepath1> <filepath2>')
   .description('Compares two configuration files and shows a difference.')
   .version('1.0.0')
-  .option('-h, --help', 'output usage information')
+  .helpOption('-h, --help', 'output usage information')
+  .option('-f, --format [type]', 'output format')
 
 program.parse();
 
