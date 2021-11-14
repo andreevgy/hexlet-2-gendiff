437266d6b0f17e89f1b97ea223edbc011e012abd
Gleb Andreev <gleb.andreev2000@gmail.com>
2021-11-14 11:40:01 +0300

    Fixed tests

--- __tests__/gendiff.test.js
+++ __tests__/gendiff.test.js
@@ -1,10 +1,14 @@
 import fs from 'fs';
 import path from 'path';
+import { fileURLToPath } from 'url';
 import genDiffJSON from '../src/gendiff';
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 test('gendiff works correctly for json files', () => {
-  const file1 = fs.readFileSync(path.resolve('./__fixtures__/file1.json'), 'utf8');
-  const file2 = fs.readFileSync(path.resolve('./__fixtures__/file2.json'), 'utf8');
+  const file1 = fs.readFileSync(path.join(__dirname, '..', '__fixtures__', 'file1.json'), 'utf8');
+  const file2 = fs.readFileSync(path.join(__dirname, '..', '__fixtures__', 'file2.json'), 'utf8');
   const expected = fs.readFileSync(path.resolve('./__fixtures__/expect_output.txt'), 'utf8');
   const result = genDiffJSON(JSON.parse(file1), JSON.parse(file2));
   expect(result).toEqual(expected);
